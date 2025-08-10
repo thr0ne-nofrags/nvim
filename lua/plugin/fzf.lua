a61cdfa12c10375481a8ad47e1274f8fc1786c88
thr0ne-nofrags <nofragsthrone@gmail.com>
2025-08-10 17:30:28 +0200

    update fzf.lua

--- lua/plugin/fzf.lua
+++ lua/plugin/fzf.lua
@@ -46,46 +46,46 @@ return {
       desc = "Search git status."
     },
     -- LSP menus
-    {
-      "<leader>flr",
-      function() require('fzf-lua').lsp_references() end,
-      desc = "Search references."
-    },
-    {
-      "<leader>fld",
-      function() require('fzf-lua').lsp_definitions() end,
-      desc = "Search definitions."
-    },
-    {
-      "<leader>flD",
-      function() require('fzf-lua').lsp_declarations() end,
-      desc = "Search declarations."
-    },
-    {
-      "<leader>fli",
-      function() require('fzf-lua').lsp_implementations() end,
-      desc = "Search implementations."
-    },
-    {
-      "<leader>fls",
-      function() require('fzf-lua').lsp_document_symbols() end,
-      desc = "Search document symbols."
-    },
-    {
-      "<leader>flS",
-     function() require('fzf-lua').lsp_live_workspace_symbols() end,
-      desc = "Search workspace symbols."
-    },
-    {
-      "<leader>fle",
-     function() require('fzf-lua').diagnostics_document() end,
-      desc = "Search dianostics in document."
-    },
-    {
-      "<leader>flE",
-     function() require('fzf-lua').diagnostics_workspace() end,
-      desc = "Search dianostics in workspace."
-    },
+    -- {
+    --   "<leader>flr",
+    --   function() require('fzf-lua').lsp_references() end,
+    --   desc = "Search references."
+    -- },
+    -- {
+    --   "<leader>fld",
+    --   function() require('fzf-lua').lsp_definitions() end,
+    --   desc = "Search definitions."
+    -- },
+    -- {
+    --   "<leader>flD",
+    --   function() require('fzf-lua').lsp_declarations() end,
+    --   desc = "Search declarations."
+    -- },
+    -- {
+    --   "<leader>fli",
+    --   function() require('fzf-lua').lsp_implementations() end,
+    --   desc = "Search implementations."
+    -- },
+    -- {
+    --   "<leader>fls",
+    --   function() require('fzf-lua').lsp_document_symbols() end,
+    --   desc = "Search document symbols."
+    -- },
+    -- {
+    --   "<leader>flS",
+    --  function() require('fzf-lua').lsp_live_workspace_symbols() end,
+    --   desc = "Search workspace symbols."
+    -- },
+    -- {
+    --   "<leader>fle",
+    --  function() require('fzf-lua').diagnostics_document() end,
+    --   desc = "Search dianostics in document."
+    -- },
+    -- {
+    --   "<leader>flE",
+    --  function() require('fzf-lua').diagnostics_workspace() end,
+    --   desc = "Search dianostics in workspace."
+    -- },
   }
 }
 
